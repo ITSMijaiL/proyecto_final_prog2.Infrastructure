ab39f2451badf6554669084f62a307fa41c6d699
ITSMijaiL <MijaiL@nowhere>
2024-06-28 22:26:28 -0400

    Add initial entities' controllers in the api + some adjustments in other parts

--- AppDBContext.cs
+++ AppDBContext.cs
@@ -3,7 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
+using proyecto_final_prog2.Domain.Entities;
 
 namespace proyecto_final_prog2.Infrastructure
 {
@@ -11,7 +12,10 @@ namespace proyecto_final_prog2.Infrastructure
     {
         public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
         {
-
         }
+
+        public DbSet<Domain.Entities.Task> tasks { get; set; }
+        public DbSet<Domain.Entities.Tag> tags { get; set; }
+        public DbSet<Domain.Entities.Column> columns { get; set; }
     }
 }
